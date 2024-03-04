25889e9c56713fc295875856773019c750af9ac3
DavideGenovese <davide.genovese1395@gmail.com>
2024-03-04 16:12:04 +0100

    Aggiunte App, header,footer e creata la homeView

--- gestoreSpese/src/App.vue
+++ gestoreSpese/src/App.vue
@@ -1,10 +1,15 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+export default{
+  components:{Header,Footer}
+}
 </script>
 
 <template>
-  
-
-  <RouterView />
+  <Header/>
+ <RouterView />
+  <Footer/>
 </template>
 
---
+++ gestoreSpese/src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="w-screen h-[80px] border-2 border-red-600"></div>
+</template>
