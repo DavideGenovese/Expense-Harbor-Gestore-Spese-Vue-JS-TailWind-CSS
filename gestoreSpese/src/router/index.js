2113386f1aa5db3cca51b48a8778ff471d824c80
AuroraPicano <aurorapicano3@gmail.com>
2024-03-06 09:31:29 +0100

    aggiunta di header e footer

--- gestoreSpese/src/router/index.js
+++ gestoreSpese/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +14,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
+      component:AboutView
     }
   ]
 })
